9c16cf152fe92cef9e8d0893d7bd2ba7958fa166
Sedef Cakmak <cakmaksedef@gmail.com>
2022-04-27 20:31:52 +0300

    wobbling animation working properly

---
+++ index.js
@@ -0,0 +1,29 @@
+// const pupilEl = document.querySelectorAll(".eye-pupil");
+
+// const wrapper = document.querySelectorAll(".pupil-wrapper");
+
+// const jiggling = [
+//   { transform: "translate(0, 0)" },
+//   { transform: "translate(1px, 1px)" },
+//   { transform: "translate(1px, 1px)" },
+//   { transform: "translate(-1px, -1px)" },
+//   { transform: "translate(1px, 1px)" },
+//   { transform: "translate(0, 0)" },
+// ];
+// const jigglingTiming = {
+//   duration: 200,
+//   iterations: Infinity,
+//   animationDelay: 5000,
+// };
+
+// (function jiggle() {
+//   wrapper.forEach((pupil) => {
+//     setTimeout(() => {
+//       pupil.classList.add("soft-jiggle");
+//     }, 2000);
+//   });
+// })();
+
+// wrapper.forEach((pupil) => {
+//   pupil.animate(jiggling, jigglingTiming);
+// });
