1dcd38b64a748c3b6ce2432bcb831656e2d6594c
Sedef Cakmak <cakmaksedef@gmail.com>
2022-04-28 00:05:13 +0300

    implementing media queries

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const pupilEl = document.querySelector("#pupil");
+
 var sound = new Howl({
   src: ["nirvana.wav"],
   loop: false,
   volume: 1,
 });
 
-// const wrapper = document.querySelectorAll(".pupil-wrapper");
 let iterationCount = 0;
-
 pupil.onanimationiteration = () => {
   iterationCount++;
-  console.log(`Animation iteration count: ${iterationCount / 2}`);
   if (iterationCount === 9) sound.play();
 };
-
-// (function jiggle() {
-//   wrapper.forEach((pupil) => {
-//     setTimeout(() => {
-//       pupil.classList.add("soft-jiggle");
-//     }, 2000);
-//   });
-// })();
